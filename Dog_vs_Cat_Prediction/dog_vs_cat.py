a539e8ace64d551264fea6e268d1f2b50e630a6d
Fecioru <birsan.lucian04@gmail.com>
2026-04-04 18:13:19 +0300

    Am adaugat primul branch

---
+++ Dog_vs_Cat_Prediction/dog_vs_cat.py
@@ -0,0 +1,139 @@
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import cv2
+
+path = r"W:\vscode\SQL\MachineLearningProject\Dog_vs_Cat_Prediction\train\train"
+
+files = glob.glob(os.path.join(path, "**/*.jpg"), recursive=True)
+file_count = len(files)
+
+#print("Total number of images: ",file_count)
+
+# all_files = []
+# categories = ['cats', 'dogs']
+
+# for category in categories:
+#     folder_path = os.path.join(path, category)
+#     files = os.listdir(folder_path)
+#     all_files.extend(files)
+#     print(f"Am găsit {len(files)} imagini în folderul '{category}'")
+
+# img_cat = mpimg.imread(r"C:\Program Files (x86)\vcodestuff\MACHINE LEARNING\Dog_vs_Cat_Prediction\dataset\train\cats\cat.2.jpg")
+# img_dog = mpimg.imread(r"C:\Program Files (x86)\vcodestuff\MACHINE LEARNING\Dog_vs_Cat_Prediction\dataset\train\dogs\dog.15.jpg")
+# imgplot = plt.imshow(img_dog)
+# plt.show()
+file_names = os.listdir(r"W:\vscode\SQL\MachineLearningProject\Dog_vs_Cat_Prediction\train\train")
+
+#print(file_names)
+
+for i in range(5):
+    name = file_names[i]
+    #(name[0:3])
+
+dog_count = 0
+cat_count = 0
+
+for img_file in file_names:
+    name = img_file[0:3]
+    if name == 'dog':
+        dog_count += 1
+    else:
+        cat_count += 1
+
+#print("Number of dog images = ",dog_count)
+#print("Number of cat images = ",cat_count)
+
+#os.mkdir(r"W:\vscode\SQL\MachineLearningProject\Dog_vs_Cat_Prediction\img_resized")
+
+original_folder = r"W:\vscode\SQL\MachineLearningProject\Dog_vs_Cat_Prediction\train\train"
+resized_folder = r"W:\vscode\SQL\MachineLearningProject\Dog_vs_Cat_Prediction\img_resized"
+
+
+file_names = os.listdir(original_folder)
+cats = []
+dogs = []
+for word in file_names:
+    if word.startswith('cat'):
+        cats.append(word)
+    else:
+        dogs.append(word) 
+selected = cats[:1000] + dogs[:1000]
+
+
+cat_counter = 0
+dog_counter = 0
+
+for file_name in selected:
+    img_path = original_folder + "\\" + file_name
+
+    #print(img_path)
+
+    img = Image.open(img_path)
+    img = img.resize((224,224))
+    img = img.convert("RGB")
+    new_img_path = resized_folder + "\\" + file_name
+    img.save(new_img_path)
+
+    if file_name.startswith("cat"):
+        cat_counter += 1
+    else:
+        dog_counter += 1
+    
+# print("Total cats resized: ",cat_counter)
+# print("Total dogs resized: ",dog_counter)
+
+# img = mpimg.imread(r"W:\vscode\SQL\MachineLearningProject\Dog_vs_Cat_Prediction\img_resized\dog.10228.jpg")
+# imgplot = plt.imshow(img)
+# plt.show()
+
+# cat -> 0; dog -> 1
+filenames = os.listdir(resized_folder)
+labels = []
+#print(filenames)
+
+for i in range(2000):
+    file_name = filenames[i]
+    label = file_name[0:3]
+
+    if label == 'dog':
+        labels.append(1)
+    else:
+        labels.append(0)
+count_cat = 0
+count_dog = 0
+for label in labels:
+    if label == 1:
+        count_dog += 1
+    else:
+        count_cat += 1
+
+# print("Cats: ",count_cat)
+# print("Dogs: ",count_dog)
+
+files = []
+image_extension = ['*.png', '*.jpg']
+for e in image_extension:
+    pattern = os.path.join(resized_folder,e)
+    found_files = glob.glob(pattern)
+    files.extend(found_files)
+data = []
+
+for file in files:
+    img = cv2.imread(file)
+    if img is not None: 
+        data.append(np.asarray(img))
+    else:
+        print(f"Nu s-a putut face transformarea fisierului: {file}")
+dog_cat_images = np.asarray(data)
+
+print(dog_cat_images)
+
+
+    
+
+
