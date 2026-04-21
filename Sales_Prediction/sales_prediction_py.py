a4b271ca74b6d9aefb388ea0815ee3976140c995
Lucian <birsan.lucian04@gmail.com>
2026-04-21 22:58:33 +0300

    Add files via upload

--- Sales_Prediction/sales_prediction_py.py
+++ Sales_Prediction/sales_prediction_py.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBRFRegressor
 from sklearn import metrics
 
-df = pd.read_csv(r"W:\vscode\SQL\MachineLearningProject\Sales_Prediction\Train.csv")
+df = pd.read_csv(r"C:\Program Files (x86)\vcodestuff\MACHINE LEARNING\Sales_Prediction\Train.csv")
 
 #print(df.head(6))
 #print(df.shape)
@@ -28,4 +28,55 @@ mode_of_outlet_size = df.pivot_table(values="Outlet_Size", columns="Outlet_Type"
 miss_values = df["Outlet_Size"].isnull()
 df.loc[miss_values, "Outlet_Size"] = df.loc[miss_values, "Outlet_Type"].apply(lambda x: mode_of_outlet_size[x])
 
-print(df.isnull().sum())
+#print(df.isnull().sum())
+#print(df.describe())
+
+#.set()
+# plt.figure(figsize=(6,6))
+# sb.displot(df["Item_Weight"])
+# plt.title("Titlt")
+#plt.show()
+
+# sb.set()
+# plt.figure(figsize=(6,6))
+# sb.displot(df["Item_Visibility"])
+# plt.title("Titlt")
+# plt.show()
+
+# sb.set()
+# plt.figure(figsize=(6,6))
+# sb.displot(df["Item_MRP"])
+# plt.title("Titlt")
+# plt.show()
+
+# sb.set()
+# plt.figure(figsize=(6,6))
+# sb.displot(df["Item_Outlet_Sales"])
+# plt.title("Titlt")
+# plt.show()
+
+# sb.set()
+# plt.figure(figsize=(6,6))
+# sb.countplot(x="Outlet_Establishment_Year", data=df)
+# plt.show()
+
+# sb.set()
+# plt.figure(figsize=(6,6))
+# sb.countplot(x="Item_Fat_Content", data=df)
+# plt.show()
+
+# sb.set()
+# plt.figure(figsize=(40,6))
+# sb.countplot(y="Item_Type", data=df)
+# plt.show()
+
+# sb.set()
+# plt.figure(figsize=(40,6))
+# sb.countplot(x="Outlet_Size", data=df)
+# plt.show()
+
+#print(df["Item_Fat_Content"].value_counts())
+
+df.replace({'Item_Fat_Content': {"low fat": "Low Fat", "LF": "Low Fat", "reg": "Regular"}},inplace=True)
+
+print(df["Item_Fat_Content"].value_counts())
