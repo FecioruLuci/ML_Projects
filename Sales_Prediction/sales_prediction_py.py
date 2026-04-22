935eec02f482a9e629e42bd8609ac92a3a4880dd
Lucian <birsan.lucian04@gmail.com>
2026-04-22 17:07:45 +0300

    Add files via upload

--- Sales_Prediction/sales_prediction_py.py
+++ Sales_Prediction/sales_prediction_py.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRFRegressor
+from xgboost import XGBRegressor
 from sklearn import metrics
 
 df = pd.read_csv(r"C:\Program Files (x86)\vcodestuff\MACHINE LEARNING\Sales_Prediction\Train.csv")
@@ -79,4 +79,47 @@ df.loc[miss_values, "Outlet_Size"] = df.loc[miss_values, "Outlet_Type"].apply(la
 
 df.replace({'Item_Fat_Content': {"low fat": "Low Fat", "LF": "Low Fat", "reg": "Regular"}},inplace=True)
 
-print(df["Item_Fat_Content"].value_counts())
+#print(df["Item_Fat_Content"].value_counts())
+
+encoder = LabelEncoder()
+
+df["Item_Identifier"] = encoder.fit_transform(df["Item_Identifier"])
+df["Item_Fat_Content"] = encoder.fit_transform(df["Item_Fat_Content"])
+df["Item_Type"] = encoder.fit_transform(df["Item_Type"])
+df["Outlet_Size"] = encoder.fit_transform(df["Outlet_Size"])
+df["Outlet_Location_Type"] = encoder.fit_transform(df["Outlet_Location_Type"])
+df["Outlet_Type"] = encoder.fit_transform(df["Outlet_Type"])
+df["Outlet_Identifier"] = encoder.fit_transform(df["Outlet_Identifier"])
+
+df["Item_Visibility"] = df["Item_Visibility"].replace(0, df["Item_Visibility"].mean())
+df["Outlet_Years"] = 2026 - df["Outlet_Establishment_Year"]
+#print(df["Item_Visibility"].value_counts())
+
+# columns_to_convert = ["Item_Identifier", "Item_Fat_Content", "Item_Type", "Outlet_Size", "Outlet_Location_Type", "Outlet_Type", "Outlet_Identifier"]
+
+# df = pd.get_dummies(df, columns=columns_to_convert)
+
+
+#print(df.head(10))
+
+X = df.drop(columns=["Item_Outlet_Sales","Item_Identifier","Outlet_Establishment_Year"], axis=1)
+Y = df["Item_Outlet_Sales"]
+#print(X)
+
+#print(Y)
+
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=2)
+
+#print(x_train.shape,x_test.shape)
+
+model = XGBRegressor(n_estimators = 100, max_depth= 3, learning_rate = 0.1, random_state = 2)
+model.fit(x_train,y_train)
+prediction = model.predict(x_train)
+r2_train = metrics.r2_score(y_train,prediction)
+
+#print(f"Acuratetea pentru train este {r2_train}")
+test_prediction = model.predict(x_test)
+r2_test = metrics.r2_score(y_test,test_prediction)
+
+print(f"Acuratetea pentru test este {r2_test}")
+#print(df["Item_MRP"].mean())
